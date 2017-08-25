0915345afa32e3a7db2b8012dc88e7107ec696ab
Yastrebov <yastrebov@macroscop.com>
2017-08-25 19:27:18 +0500

    pre detector - classifier

--- classifier/cnn_server.py
+++ classifier/cnn_server.py
@@ -19,6 +19,9 @@ from socket import *
 h = 60
 w = 32 #67
 
+head_detector_width = 32
+head_detector_height = 32
+
 img_size = w*h*3
 buf_size = img_size*10
 host = 'localhost'
@@ -26,7 +29,49 @@ port = 123
 addr = (host,port)
 patt_num=0
 
-def matchPatterns(img0):
+def classifyHead(img, headDetectorModel, clsModels):
+	(rx,ry) = applySlidingDetector(img, headDetectorModel)
+	
+	img1 = img[ry:ry+head_detector_height, rx:rx+head_detector_width]
+	cv2.imwrite('head_res.png',img1)
+	
+	return (rx,ry)
+
+def applySlidingDetector(img, model):
+	rx = -1
+	ry = -1
+	h,w,c = img.shape
+	
+	maxres = 0
+	y = 0
+	while (y <= h - head_detector_height):
+		x = 0
+		while (x <= w - head_detector_width):	
+			images = []
+			np_images = []	
+			
+			img1 = img[y:y+head_detector_height, x:x+head_detector_width]				
+			
+			img1 = img1.transpose((2,0,1)) 
+			img1 = np.expand_dims(img1, axis=0)
+	
+			images.append(img1)
+	
+			np_images = np.vstack(images)
+			res = model.predict(np_images)
+	
+			#print("x={} y={} res={}".format(x,y,res1[0][0]))
+			if (res[0][0] > maxres):
+				maxres = res
+				rx = x
+				ry = y
+						
+			x = x + 4		
+		y = y + 4
+	return (rx,ry)
+
+#send full image in classifier
+def matchPatternsSimple(img0):
 	h,w,c = img0.shape
 	global patt_num
 	global models
@@ -66,10 +111,14 @@ def matchPatternsFromFile(imagefn):
 	img = cv2.imread(imagefn)
 	img = img.astype(float)/255	
 	
-	match = matchPatterns(img)
-	return match	
+	#match = matchPatternsSimple(img)
+	#return match
+	(rx,ry)=classifyHead(imgF, headModels, model)
+	return (rx,ry)	
 
 def matchFromBytes(buf):
+	global models
+	global headModel
 	global patt_num
 	if len(buf) != img_size:
 		return 0
@@ -83,8 +132,10 @@ def matchFromBytes(buf):
 				i = i+1
 		
 	imgF = img.astype(float)/255.0	 
-	v = matchPatterns(imgF)
-	return v
+	#v = matchPatternsSimple(imgF)
+	#return v
+	(rx,ry)=classifyHead(imgF, headModels, model)
+	return (rx,ry)
 	
 def loadModels():
 	modles = []
@@ -113,9 +164,13 @@ def splitData(data):
 	
 	
 #===== main =====
-#model1 = load_model('nn1.h5')
+hdmodel = load_model('head.h5')
 models = []
 models = loadModels()
+
+matchPatternsFromFile(sys.argv[1])
+
+quit()
 
 tcp_socket = socket(AF_INET, SOCK_STREAM)
 tcp_socket.bind(addr)
@@ -136,10 +191,10 @@ while True:
 		imgs = splitData(data)
 		answers = []
 		for img in imgs:			
-			v1 = matchFromBytes(img)
-			print("img done: {}".format(v1))
-			a = int(v1*100+0.5)
-			answers.append(a)
+			#v1 = matchFromBytes(img)
+			#print("img done: {}".format(v1))
+			#a = int(v1*100+0.5)
+			#answers.append(a)
 			
 		b = bytes(answers)
 		
